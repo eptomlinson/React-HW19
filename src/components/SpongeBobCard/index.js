cafbc637c6f86dbeb2864c78a1fb3fd259796c48
Erik Tomlinson <eptomlinson@gmail.com>
2020-11-17 19:45:06 -0600

    added components folder from spongebob, uncommented app js

---
+++ src/components/SpongeBobCard/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./style.css";
+
+function SpongeBobCard(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img
+          alt={props.info.name}
+          src= {props.info.image}
+        />
+      </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Name:</strong> {props.info.name}
+          </li>
+          <li>
+            <strong>Occupation:</strong> {props.info.occupation}
+          </li>
+          <li>
+            <strong>Location:</strong> {props.info.location}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default SpongeBobCard;
