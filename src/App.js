e18c8c09300072e6cc47abecec3a5ec1fcfa0008
Erik Tomlinson <eptomlinson@gmail.com>
2020-11-17 19:34:17 -0600

    added App js file in src folder from spongebob act

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import React from "react";
+// import Wrapper from "./components/Wrapper";
+// import Title from "./components/Title";
+// import SpongeBobCard from "./components/SpongeBobCard";
+// import SquidwardCard from "./components/SquidwardCard";
+// import MrKrabsCard from "./components/MrKrabsCard";
+// import friends from "./friends.json";
+
+// function App() {
+//   return (
+//     <Wrapper>
+//       <Title>Friends List</Title>
+//       <SpongeBobCard info = {friends[0]}/>
+//       <MrKrabsCard info = {friends[1]}/>
+//       <SquidwardCard info = {friends[2]}/>
+//     </Wrapper>
+//   );
+// }
+
+// export default App;
